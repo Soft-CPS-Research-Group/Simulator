7eb5e31b5a908f14a152993aa07d35f711f4d6cb
Tiago Fonseca <calof@isep.ipp.pt>
2026-03-18 01:03:05 +0000

    Prepare upstream publish metadata for CityLearn 2.6.0b1
    Signed-off-by: Tiago <calof@isep.ipp.pt>

--- citylearn/data.py
+++ citylearn/data.py
@@ -51,8 +51,8 @@ def get_settings():
 class DataSet:
     """CityLearn input data set and schema class."""
 
-    GITHUB_ACCOUNT = os.getenv('CITYLEARN_DATASET_GITHUB_ACCOUNT', 'Soft-CPS-Research-Group')
-    REPOSITORY_NAME = os.getenv('CITYLEARN_DATASET_REPOSITORY', 'Simulator')
+    GITHUB_ACCOUNT = os.getenv('CITYLEARN_DATASET_GITHUB_ACCOUNT', 'intelligent-environments-lab')
+    REPOSITORY_NAME = os.getenv('CITYLEARN_DATASET_REPOSITORY', 'CityLearn')
     REPOSITORY_TAG = os.getenv('CITYLEARN_DATASET_TAG', f'v{__version__}')
     OFFLINE = _parse_env_bool('CITYLEARN_OFFLINE', False)
     REPOSITORY_DATA_PATH = FileHandler.join_url('data')
